5f341a4cada4fee419f11b614de4b2a51730e876
Mukulika <mukulikapahari@gmail.com>
2021-04-14 09:54:05 +0530

    form validation p1

--- forms.py
+++ forms.py
@@ -1,16 +1,17 @@
 from flask_wtf import FlaskForm
 from wtforms import StringField, SubmitField
-from wtforms.validators import DataRequired, Length
+from wtforms.validators import DataRequired, Length, Optional
 
 class InputForm(FlaskForm):
     epd_name = StringField(
         'If your promoter exists in the EPD database, please enter the promoter name from EPD: ',
-        
+        [Optional()]
     )
 
     nucleotide = StringField(
         "Don't know EPD name? Enter 60 nucleotide sequence: ",
-        [Length(max=60, message=('Incorrect Sequence!'))]
+        [Optional(),
+        Length(min=60, max=60, message=('Incorrect Sequence!'))]
     )
 
     submit = SubmitField('Submit')
