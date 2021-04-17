fe2cf483840e83e60ebe1f2147acea6a2f3b53eb
Mukulika <mukulikapahari@gmail.com>
2021-04-17 10:49:01 +0530

    design upgrade

--- static/result.js
+++ static/result.js
@@ -8,20 +8,6 @@ var myScore = document.getElementById("hidden").value
 
 //Graph shitt
 
-// set the dimensions and margins of the graph
-var margin = {top: 10, right: 30, bottom: 30, left: 40},
-    width = 900 - margin.left - margin.right,
-    height = 500 - margin.top - margin.bottom;
-
-// append the svg object to the body of the page
-var svg = d3.select("#chart2")
-  .append("svg")
-    .attr("width", width + margin.left + margin.right)
-    .attr("height", height + margin.top + margin.bottom)
-  .append("g")
-    .attr("transform",
-          "translate(" + margin.left + "," + margin.top + ")");
-
 //for line graph of promoter scores
 d3.csv('static/fixed_promoters.csv', function(promoters){
     labels = promoters.map(function(d) {return d.promoter_name});
@@ -62,8 +48,9 @@ function makeChart(labels, datapoints){
         type: 'line',
         data,
         options: {
+          maintainAspectRatio: false,
+          responsive: true,
           plugins: {
-            responsive: true,
             title: {
                 display: true,
                 text: 'Scores of Standard Promoters',
